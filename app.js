89e91c638865138bfd2b729fce7f63982d62ff21
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-05-02 23:19:50 -0500

    Paso 5: Agregando nuevo route /launchx

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.send("Hola a todos mis explorers")
 })
 
+//localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+    res.send("Bienvenidos a LaunchX")
+})
+
 app.listen(port, () => {
     console.log("Server Listo!")
 })
