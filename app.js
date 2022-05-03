8f2cf716335ac4bece32cd49a747393025b7bd6d
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-05-02 23:32:14 -0500

    Paso 7: Agregando route /explorers: explorer y recibiendo query params

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ app.get('/', (req, res) => {
     res.send("Hola a todos mis explorers")
 })
 
-//localhost:3000/launchx
+// localhost:3000/launchx
 app.get('/launchx', (req, res) => {
     res.send("Bienvenidos a LaunchX")
 })
 
-app.get('/node', (req, res) => {
-    const explorer1 = { id: 1, name: "Explorer1" }
-    const explorer2 = { id: 2, name: "Explorer2" }
-    const explorer3 = { id: 3, name: "Explorer3" }
-    res.send([explorer1, explorer2, explorer3])
+// app.get('/node', (req, res) => {
+//     const explorer1 = { id: 1, name: "Explorer 1" }
+//     const explorer2 = { id: 2, name: "Explorer 2" }
+//     const explorer3 = { id: 3, name: "Explorer 3" }
+//     res.send([explorer1, explorer2, explorer3])
+// })
+
+// Query Params
+// localhost/explorers/mauriicioreyes
+app.get('/explorers/:explorer', (req, res) => {
+    console.log(req.params) // { explorer: 'mauricio' }
+    res.send(req.params)
 })
 
 app.listen(port, () => {
